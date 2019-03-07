8bb5f303f3172164ec9c6b873f05dd65f3fb08ce
Jamesszabo <46973312+Jamesszabo@users.noreply.github.com>
2019-03-07 15:18:44 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
-
+printf("Print in order\n"); 
+printinorder(&bt);
+printf("\nPrint pre order\n"); 
+printf("\nPrint post order\n"); 
+printpostorder(&bt); 
+printf("\n");
+found = search(&bt,33);
+if (found){
+printf("FOUND 33\n");
+}
+found = search(&bt,38);
+if (found){ 
+printf("FOUND 38\n");
+}
+printf("Number of elements in tree: %d\n", btsize(&bt)); 
+printf("Tree height: %d\n", treeheight(&bt)); 
+  
