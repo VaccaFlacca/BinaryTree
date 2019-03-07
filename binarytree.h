424dc7f535f5e5a99c5a6fbd6211344cbbd9a650
Cody Vacca <31662956+VaccaFlacca@users.noreply.github.com>
2019-03-07 15:06:49 -0700

    Update binarytree.h

--- binarytree.h
+++ binarytree.h
@@ -1,1 +1,9 @@
 
+void initialize(binary_tree* bt);
+bool search(binary_tree* bt, int key);
+void insert(binary_tree* bt, int item);
+void printinorder(binary_tree* bt);
+void printpreorder(binary_tree* bt);
+void printpostorder(binary_tree* bt);
+int btsize(binary_tree* bt);
+int treeheight(binary_tree* bt);
