8dd298e5db4f33bf8fac6b0eb142315c2e81edd4
MediocreCode <44242502+MediocreCode@users.noreply.github.com>
2019-03-07 15:18:09 -0700

    Added structs

--- binarytree.c
+++ binarytree.c
@@ -1,1 +1,12 @@
 
+typedef struct node_struct{
+    int data;
+    struct node_struct* left;
+    struct node_struct* right;
+}node;
+
+typedef struct bt{
+    node* root;
+    int size;
+    int height;
+}bt;
