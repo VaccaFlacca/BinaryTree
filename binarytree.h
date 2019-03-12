b6ffc51cc5f9c28526917101a352a88fb0c6c1c5
Cody Vacca <31662956+VaccaFlacca@users.noreply.github.com>
2019-03-12 14:10:53 -0600

    added structs

--- binarytree.h
+++ binarytree.h
@@ -1,6 +1,18 @@
 #ifndef binarytree_h
 #define binarytree_h
 
+typedef struct node_struct{
+    int data;
+    struct node_struct* left;
+    struct node_struct* right;
+}node;
+
+typedef struct binary_tree{
+    node* root;
+    int size;
+    int height;
+}bt;
+
 void initialize(binary_tree* bt);
 bool search(binary_tree* bt, int key);
 void insert(binary_tree* bt, int item);
