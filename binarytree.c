b329b71398fe5b8c309d2a770e20717aa660fe17
Cody Vacca <31662956+VaccaFlacca@users.noreply.github.com>
2019-03-12 14:10:24 -0600

    move structs to h

--- binarytree.c
+++ binarytree.c
@@ -1,15 +1,5 @@
 
-typedef struct node_struct{
-    int data;
-    struct node_struct* left;
-    struct node_struct* right;
-}node;
 
-typedef struct binary_tree{
-    node* root;
-    int size;
-    int height;
-}bt;
 
 void initialize(binary_tree* bt){
     
