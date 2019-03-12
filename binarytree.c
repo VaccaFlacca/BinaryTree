98abf64fa091c7bb5bf3b5d2fd52174e27e865b9
EvanWilken <44215938+EvanWilken@users.noreply.github.com>
2019-03-12 14:30:06 -0600

    Update binarytree.c
    Added insert

--- binarytree.c
+++ binarytree.c
@@ -7,9 +7,51 @@ void initialize(binary_tree* bt){
 
 bool search(binary_tree* bt, int key){
     
+    
+    
 }
 
-void insert(binary_tree* bt, int item){
+void insert(binary_tree* bt, node item)
+{
+    if (bt->root == NULL)
+    {
+        tree->root = item;
+        item->left = NULL;
+        item->right = NULL;
+    }
+    else 
+    {
+        node current = tree->root;
+        while (current != NULL)
+        {
+            if (item->data < current->data)
+            {
+                if (current->left == NULL)
+                {
+                    current->left = item;
+                    current = NULL;
+                }
+                else 
+                {
+                    current = current->left
+                }
+            }
+            else
+            {
+                if (current->right == NULL)
+                {
+                    current->right = NULL;
+                    current = NULL;
+                }
+                else
+                {
+                    current = current->right;
+                }
+            }
+            item->left = NULL;
+            item->right = NULL;
+        }
+    }
     
 }
 
