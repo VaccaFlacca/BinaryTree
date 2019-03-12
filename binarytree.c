51448d9257a4a40d81d72710dae6ccfc9db971f5
Cody Vacca <31662956+VaccaFlacca@users.noreply.github.com>
2019-03-12 12:23:17 -0600

    added functions to be defined

--- binarytree.c
+++ binarytree.c
@@ -10,3 +10,35 @@ typedef struct bt{
     int size;
     int height;
 }bt;
+
+void initialize(binary_tree* bt){
+    
+}
+
+bool search(binary_tree* bt, int key){
+    
+}
+
+void insert(binary_tree* bt, int item){
+    
+}
+
+void printinorder(binary_tree* bt){
+    
+}
+
+void printpreorder(binary_tree* bt){
+    
+}
+
+void printpostorder(binary_tree* bt){
+    
+}
+
+int btsize(binary_tree* bt){
+    
+}
+
+int treeheight(binary_tree* bt){
+    
+}
