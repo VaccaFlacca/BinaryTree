7acba0531d6acdf539fa053e91d40884cb371de2
Jamesszabo <46973312+Jamesszabo@users.noreply.github.com>
2019-03-14 15:01:40 -0600

    Update binarytree.h

--- binarytree.h
+++ binarytree.h
@@ -16,13 +16,13 @@ typedef struct binary_tree{
 
 void initialize(binary_tree* bt);
 bool search(binary_tree* bt, int key);
-void insert(binary_tree* bt, node item);
+void insert(binary_tree* bt, node *item);
 void printinorder(binary_tree* bt);
 void inorder(node item);
 void printpreorder(binary_tree* bt);
 void preorder(node item);
 void printpostorder(binary_tree* bt);
-void postorder(node item);
+void postorder(node *item);
 int btsize(binary_tree* bt);
 int treeheight(binary_tree* bt);
 
