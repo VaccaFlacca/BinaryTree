b3935bda27786d7f9f96b218a663796067ded3e1
EvanWilken <44215938+EvanWilken@users.noreply.github.com>
2019-03-14 14:15:21 -0600

    Update binarytree.h
    changed insert from passing in int to node

--- binarytree.h
+++ binarytree.h
@@ -15,7 +15,7 @@ typedef struct binary_tree{
 
 void initialize(binary_tree* bt);
 bool search(binary_tree* bt, int key);
-void insert(binary_tree* bt, int item);
+void insert(binary_tree* bt, node item);
 void printinorder(binary_tree* bt);
 void inorder(node item);
 void printpreorder(binary_tree* bt);
